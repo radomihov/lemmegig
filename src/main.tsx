6bbfa09f3449126fd3cc2d9c3f8f49c54ce2ac6e
Radoslav Mihov <radoslav.mihov@gmail.com>
2024-07-30 09:06:23 +0300

    Add environments to test

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import App from './App.tsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
 
+
+const baseUrl=import.meta.env.VITE_API_BASE_URL;
+console.log(baseUrl);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
       <React.StrictMode>
