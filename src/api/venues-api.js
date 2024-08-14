362b9852b7510d330caf9a9c7ca653957e477ca3
Radoslav Mihov <radoslav.mihov@gmail.com>
2024-08-14 13:48:08 +0300

    Fix call to artists

--- src/api/venues-api.js
+++ src/api/venues-api.js
@@ -2,7 +2,7 @@ import * as request from './requester';
 import {BASE_URL} from "./requester";
 
 export const getAll = () => request.get(`${BASE_URL}/venues`);
-export const getOne = (artistId) => request.get(`${BASE_URL}/venues/${artistId}`);
+export const getOne = (venueId) => request.get(`${BASE_URL}/venues/${venueId}`);
 
 export const create = (name, image, address, phone, email, description) => request.post(`${BASE_URL}/venues`, {name, image, address, phone, email, description} );
 
